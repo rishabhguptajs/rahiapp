9c1aff9d4dd9f1f8c7623f847f001c21d1d225d4
rishabhguptajs <rishabhgupta4523@gmail.com>
2025-01-18 18:46:03 +0530

    Add initial configuration files and dependencies for Next.js application
    - Created next.config.mjs to configure image optimization settings.
    - Added package.json and package-lock.json to manage project dependencies, including Tailwind CSS, PostCSS, and various React libraries.
    - Introduced postcss.config.mjs for PostCSS configuration with Tailwind CSS.
    - Established tailwind.config.js for Tailwind CSS setup, including custom animations and colors.
    - Configured TypeScript with tsconfig.json for type checking and module resolution.

---
+++ client/tailwind.config.js
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+const colors = require("tailwindcss/colors")
+const {
+  default: flattenColorPalette
+} = require("tailwindcss/lib/util/flattenColorPalette")
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
+      colors: {
+        customOrange: "#FA7436",
+        customgrey: "#F7F8FC",
+      },
+    }
+  },
+  plugins: [addVariablesForColors]
+}
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"))
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  )
+
+  addBase({
+    ":root": newVars
+  })
+}
